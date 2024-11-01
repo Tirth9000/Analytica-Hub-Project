e8a9cfcccf559e9a8667cadcff777705600a8c49
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2024-11-01 04:39:33 +0000

    home page modified

---
+++ static/landing.js
@@ -0,0 +1,23 @@
+// script.js
+
+// Highlight the active section in navbar
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('.section');
+    const navLinks = document.querySelectorAll('.nav-link');
+  
+    let currentSection = '';
+  
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      if (pageYOffset >= sectionTop - 60) {
+        currentSection = section.getAttribute('id');
+      }
+    });
+  
+    navLinks.forEach(link => {
+      link.classList.remove('active');
+      if (link.getAttribute('href').substring(1) === currentSection) {
+        link.classList.add('active');
+      }
+    });
+  });
