3ac6861e7c988d8463b3671cea8e096f54e0b1f5
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2024-12-12 08:55:50 +0000

    modified docker and project frontend.

--- analytica_app/views.py
+++ analytica_app/views.py
@@ -4,3 +4,11 @@ from templates import *
 # Create your views here.
 def LandingPage(request):
     return render(request, 'landing.html')
+
+
+def AnalyticPage(request):
+    return render(request, 'analytic.html')
+
+def UploadFile(request):
+    return render(request, 'upload_file.html')
+
