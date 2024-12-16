87a9304fad3776abc7f10e064aa79d5a1b7e1ce5
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2024-12-16 20:46:56 +0000

    trying to connect mongodb in django models.

--- analytica_app/models.py
+++ analytica_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from mongoengine import *
 
 # Create your models here.
 class AnalysisFile(models.Model):
@@ -9,3 +10,17 @@ class AnalysisFile(models.Model):
 
     def __str__(self):
         return self.file_id
+
+
+# class User(Document):
+#     file_id = StringField(primary_key=True, max_length=6)
+#     file = FileField()
+#     uploaded_at = DateTimeField(auto_now_add=True)
+#     updated_at = DateTimeField(auto_now=True)
+
+#     name = StringField(required=True, max_length=50)
+#     email = EmailField(required=True, unique=True)
+#     age = IntField(min_value=0)
+
+#     def __str__(self):
+#         return self.name
