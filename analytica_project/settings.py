c585c2fc5515e21f6b987ae2e0fc48100a3ac654
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2024-12-22 16:06:01 +0000

    modified db error files.

--- analytica_project/settings.py
+++ analytica_project/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_mongoengine',
-    'django_mongoengine.admin',
     'analytica_app',
 ]
 
