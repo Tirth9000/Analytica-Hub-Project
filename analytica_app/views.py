25c09db3c6b042199dffc5ca6702dae55c510e66
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2025-01-01 20:18:05 +0000

    modified data file preview from database.

--- analytica_app/views.py
+++ analytica_app/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from templates import *
+from .models import AnalysisFile
 
 # Create your views here.
 def LandingPage(request):
     return render(request, 'landing.html')
 
-
-def AnalyticPage(request):
-    return render(request, 'analytic.html')
+def AnalyticPage(request, id):
+    dataFile = AnalysisFile.objects.get(file_id = id)
+    return render(request, 'analytic.html', {"file": dataFile})
 
 def UploadFile(request):
-    return render(request, 'upload_file.html')
+    files = AnalysisFile.objects.all()
+    return render(request, 'upload_file.html', {"files": files})
 
