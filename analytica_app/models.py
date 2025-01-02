28b0279161fd22c5bf667d6f3b7f5420c0ef1457
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2025-01-02 21:00:14 +0000

    modify the models field, analytic frontend and some analytic feature.

--- analytica_app/models.py
+++ analytica_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class AnalysisFile(models.Model):
     file_id = models.CharField(primary_key=True, max_length=6)
+    file_name = models.CharField(max_length=40, default="New Document")
     file = models.FileField(upload_to="analysis_files/")
     uploaded_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
