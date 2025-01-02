9aa9b894336324955d67fd4f1502c8d9c7a3669d
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2025-01-02 11:57:15 +0000

    updated analytic page with data preview

--- analytica_app/views.py
+++ analytica_app/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, redirect
 from templates import *
 from .models import AnalysisFile
+import pandas as pd
 
 # Create your views here.
 def LandingPage(request):
     return render(request, 'landing.html')
 
 def AnalyticPage(request, id):
-    dataFile = AnalysisFile.objects.get(file_id = id)
-    return render(request, 'analytic.html', {"file": dataFile})
+    # dataFile = AnalysisFile.objects.get(file_id = id)
+    dataFile = 'hello'
+    datafile = pd.read_csv('shopping_trends.csv')
+    columns = datafile.columns.tolist()
+    rows = datafile.values.tolist()
+    return render(request, 'analytic.html', {"file": dataFile, "columns": columns, "rows": rows})
 
 def UploadFile(request):
     files = AnalysisFile.objects.all()
